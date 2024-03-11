ad43bc004aec3fbe0515cf3ba40c0d6bbf031fd5
mayur1-gupta <144005425+mayur1-gupta@users.noreply.github.com>
2024-03-11 10:32:41 +0530

    Update Counter.jsx

--- my_app/src/Counter.jsx
+++ my_app/src/Counter.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { counterAction,Selector } from './Redux/Reducer'
+import { Action,Selector } from './Redux/Reducer'
 import { useDispatch,useSelector } from 'react-redux'
 import './App.css'
 const Counter = () => {
     const dispach = useDispatch()
     const count = useSelector(Selector)
 
-    function HandleIncrease(){
-        dispach(counterAction.Increment())
+    function HandleInc(){
+        dispach(Action.Increment())
     }
 
-    function HandleDecrement(){
-        dispach(counterAction.Decrement())
+    function HandleDec(){
+        dispach(Action.Decrement())
     }
     return (
     <div className='body'>
       <h1>{count}</h1>
-      <button onClick={HandleIncrease}>Like</button>
-      <button onClick={HandleDecrement}>Unlike</button>
+      <button onClick={HandleInc}>Like</button>
+      <button onClick={HandleDec}>Unlike</button>
     </div>
   )
 }

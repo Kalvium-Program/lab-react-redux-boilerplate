4be0b7443f966ae3f912ed6f24d9d4b777315fdf
mayur1-gupta <144005425+mayur1-gupta@users.noreply.github.com>
2024-03-11 10:33:14 +0530

    Update App.jsx

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
 import { Provider } from 'react-redux'
-import {mystore} from './Redux/Store'
+import {myCountstore} from './Redux/Store'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Provider store ={mystore}>
+    <Provider store ={myCountstore}>
       <Counter />   
     </Provider>
     </>

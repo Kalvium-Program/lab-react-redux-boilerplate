4c4c7d91313b48c741b025d3def4ead358af832e
Mayur_Gupta <144005425+mayur1-gupta@users.noreply.github.com>
2024-03-18 10:59:10 +0530

    Update Reducer.jsx

--- my_app/src/Redux/Reducer.jsx
+++ my_app/src/Redux/Reducer.jsx
@@ -1,14 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const CreateSlice = createSlice({
-    Countername:'counter',
-    initialState:0,
-    reducers:{
-        Increment:(state)=>state+1,
-        Decrement:(state)=>state-1
-    }
-})
+// const CreateSlice = createSlice({
+//     Countername:'counter',
+//     initialState:0,
+//     reducers:{
+//         Increment:(state)=>state+1,
+//         Decrement:(state)=>state-1
+//     }
+// })
 
-export const Action = CreateSlice.actions
-export const Reducer = CreateSlice.reducer
-export const Selector = (state)=> state.Reducer
+// export const Action = CreateSlice.actions
+// export const Reducer = CreateSlice.reducer
+// export const Selector = (state)=> state.Reducer

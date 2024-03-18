b834d3a3afbcf5798dc8b6f91b8d95b46884461b
Mayur_Gupta <144005425+mayur1-gupta@users.noreply.github.com>
2024-03-18 10:37:24 +0530

    Update Counter.jsx

--- my_app/src/Counter.jsx
+++ my_app/src/Counter.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import { Action,Selector } from './Redux/Reducer'
-import { useDispatch,useSelector } from 'react-redux'
-import './App.css'
+import React from 'react';
+import { Increment, Decrement, Selector } from './Redux/Reducer'; // Import Increment, Decrement directly
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+
 const Counter = () => {
-    const dispach = useDispatch()
-    const count = useSelector(Selector)
+    const dispatch = useDispatch(); // Corrected typo in variable name
+    // const count = useSelector(Selector);
 
-    function HandleInc(){
-        dispach(Action.Increment())
+    // function handleInc() {
+    //     dispatch(Increment());
+    // }
+
+    // function handleDec() {
+    //     dispatch(Decrement());
+    // }
+
+    const [count, setCount] = React.useState(0);
+    const handleInc = () => {
+        setCount(count + 1);
+    }
+    const handleDec = () => {
+        setCount(count - 1);
     }
 
-    function HandleDec(){
-        dispach(Action.Decrement())
-    }
+
+    console.log(count);
     return (
-    <div className='body'>
-      <h1>{count}</h1>
-      <button onClick={HandleInc}>Like</button>
-      <button onClick={HandleDec}>Unlike</button>
-    </div>
-  )
+        <div className='body'>
+            <h1>{count}</h1> {/* Display the count */}
+            <button onClick={handleInc}>Like</button>
+            <button onClick={handleDec}>Unlike</button>
+        </div>
+    );
 }
 
-export default Counter
+export default Counter;
